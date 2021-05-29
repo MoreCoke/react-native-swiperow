dd5c1e8a3a762445bebb8518add4b9b337ae368d
MoreCoke <seed45699@gmail.com>
2021-05-29 23:19:33 +0800

    Structure project file folder
    app 資料夾和 app.json 撞名造成的奇怪 bug
    https://github.com/facebook/react-native/issues/12539#issuecomment-282610740
    
    https://stackoverflow.com/questions/34130539/uncaught-error-invariant-violation-element-type-is-invalid-expected-a-string

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './app/index.js';
 import {name as appName} from './app.json';
-
+console.log('DD', App);
 AppRegistry.registerComponent(appName, () => App);
