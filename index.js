431982d746de5c5e808510d924dedc122c00ac0e
MoreCoke <seed45699@gmail.com>
2021-05-31 02:16:32 +0800

    Install react-native-gesture-handler

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
  */
 
 import {AppRegistry} from 'react-native';
+import 'react-native-gesture-handler';
+
 import App from './app/index.js';
 import {name as appName} from './app.json';
-console.log('DD', App);
 AppRegistry.registerComponent(appName, () => App);
